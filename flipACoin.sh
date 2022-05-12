15ae7d527cef8d69a4c298a961a1196519d36477
Avani14 <trivediavani14@gmail.com>
2022-05-12 16:50:53 +0530

    4th commit

--- flipACoin.sh
+++ flipACoin.sh
@@ -1,1 +1,22 @@
 echo "Flip a coin.."
+countH=0
+countT=0
+while [[ $countH -lt 21 && $countT -lt 21 ]]
+do
+	n=$((RANDOM%2))
+	if [[ $n -eq 0 ]]
+	then
+		countH=$(($countH+1))
+	else
+		countT=$(($countT+1))
+	fi
+done
+if [[ $countH -gt $countT ]]
+then
+	echo "Head wins : $countH times"
+elif [[ $countT -gt $countH ]]
+then
+	echo "Tail wins : $countT times"
+else
+	echo "Its a tie"
+fi
