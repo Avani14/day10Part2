941e2c113d32e0ab7e10cda5013e3b987dc76081
Avani14 <trivediavani14@gmail.com>
2022-05-12 16:43:27 +0530

    3 commit

--- flipACoin.sh
+++ flipACoin.sh
@@ -1,1 +1,16 @@
 echo "Flip a coin.."
+countH=0
+countT=0
+for ((i=0;i<=10;i++))
+do
+	n=$((RANDOM%2))
+	if [[ $n -eq 0 ]]
+	then
+		countH=$(($countH+1))
+	else
+		countT=$(($countT+1))
+	fi
+done
+
+echo "Head wins : $countH times"
+echo "Tail wins : $countT times"
